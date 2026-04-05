c1a0d99f82c07f13984ba726788fe37f2801cc01
Pushpendramaurya2026 <mauryapushpendra6@gmail.com>
2026-04-05 23:16:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,27 @@
-const items = document.querySelectorAll('.gallery-item');
-const filterBtns = document.querySelectorAll('.filter-btn');
-const lightbox = document.getElementById('lightbox');
-const lightboxImg = document.getElementById('lightbox-img');
-const closeBtn = document.querySelector('.close-btn');
-const nextBtn = document.getElementById('nextBtn');
-const prevBtn = document.getElementById('prevBtn');
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-let currentIndex = 0;
-let visibleImages = [...items]; // Tracks currently filtered images
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
 
-// 1. Filtering Logic
-filterBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        // Update active button
-        document.querySelector('.active').classList.remove('active');
-        btn.classList.add('active');
-
-        const filter = btn.getAttribute('data-filter');
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
         
-        visibleImages = []; // Reset visible list
-        items.forEach(item => {
-            if (filter === 'all' || item.getAttribute('data-category') === filter) {
-                item.style.display = 'block';
-                visibleImages.push(item);
-            } else {
-                item.style.display = 'none';
-            }
-        });
-    });
-});
-
-// 2. Lightbox Navigation
-const showImage = (index) => {
-    if (index >= visibleImages.length) index = 0;
-    if (index < 0) index = visibleImages.length - 1;
-    currentIndex = index;
-    const imgUrl = visibleImages[currentIndex].querySelector('img').src;
-    lightboxImg.src = imgUrl;
-    lightbox.style.display = 'flex';
-};
-
-// Open on click
-items.forEach((item) => {
-    item.addEventListener('click', () => {
-        const indexInVisible = visibleImages.indexOf(item);
-        showImage(indexInVisible);
-    });
-});
-
-// Close
-closeBtn.onclick = () => lightbox.style.display = 'none';
-
-// Next/Prev
-nextBtn.onclick = () => showImage(currentIndex + 1);
-prevBtn.onclick = () => showImage(currentIndex - 1);
-
-// Close on background click
-lightbox.onclick = (e) => {
-    if (e.target === lightbox) lightbox.style.display = 'none';
-};
+    })
+})
